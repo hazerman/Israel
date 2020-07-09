f47b59e4e3d404429e597e1d2f7e3227a924213a
hazerman <49827421+hazerman@users.noreply.github.com>
2020-07-09 18:48:05 +0500

    Исправляет проблемы сборки

--- gulpfile.js
+++ gulpfile.js
@@ -72,8 +72,10 @@ gulp.task("html", function () {
 });
 
 gulp.task("copy", function() {
-  return gulp
-    .src(["source/fonts/**/*.{woff,woff2}"], {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    ], {
       base: "source"
     })
     .pipe(gulp.dest("build"));
@@ -110,7 +112,6 @@ gulp.task(
     "copy",
     "css",
     "js",
-    "images",
     "webp",
     "sprite",
     "html"
