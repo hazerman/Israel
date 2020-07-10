52a9be49dbc4cdf88953665064ca7b689b408a46
hazerman <49827421+hazerman@users.noreply.github.com>
2020-07-10 19:05:59 +0500

    Обновляет build

--- build/js/main.js
+++ build/js/main.js
@@ -1,15 +1,86 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+(function () {
+  var popupForm = document.querySelector('#popup-form');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+  popupForm.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    window.popup.remove();
+  });
+})();
+
+'use strict';
+
+(function () {
+  var SHOWN_POPUP = 'popup--show';
+  var popupRequest = document.querySelector('#request');
+  var popupAccept = document.querySelector('#accepted');
+  var currentPopup;
+  var closeButton;
+  var acceptButton;
+
+  var callButton = document.querySelector('.header__info button');
+
+  var removePopup = function () {
+    currentPopup.classList.remove(SHOWN_POPUP);
+    closeButton.removeEventListener('click', onCloseButtonClick);
+    document.removeEventListener('keydown', onEscPress);
+    closeButton = null;
+    if (currentPopup === popupRequest) {
+      currentPopup = null;
+      showPopup(popupAccept);
+    } else {
+      currentPopup = null;
+    }
+  };
+
+  var onCloseButtonClick = function () {
+    removePopup();
+  };
+
+  var onAcceptButtonClick = function () {
+    removePopup();
+    acceptButton.removeEventListener('click', onAcceptButtonClick);
+    acceptButton = null;
+  };
+
+  var onEscPress = function (evt) {
+    window.util.escEvent(evt, removePopup);
+  };
+
+  var showPopup = function (popup) {
+    currentPopup = popup;
+    currentPopup.classList.add(SHOWN_POPUP);
+    closeButton = currentPopup.querySelector('.popup__close-btn');
+    closeButton.addEventListener('click', onCloseButtonClick);
+    if (currentPopup.querySelector('.popup__button')) {
+      acceptButton = currentPopup.querySelector('.popup__button');
+      acceptButton.addEventListener('click', onAcceptButtonClick);
+    }
+    document.addEventListener('keydown', onEscPress);
+  };
+
+  callButton.addEventListener('click', function () {
+    showPopup(popupRequest);
+  });
+
+  window.popup = {
+    remove: removePopup
+  };
+})();
+
+'use strict';
+
+(function () {
+  var KEY_ESC = 27;
+
+  var elementEscPressHadler = function (evt, callback) {
+    if (evt.keyCode === KEY_ESC) {
+      callback();
+    }
+  };
+
+  window.util = {
+    escEvent: elementEscPressHadler
+  };
+})();
