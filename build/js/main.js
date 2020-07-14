ab7993ef9a47eaba7f86f0c22d6f17e1cbc15fe2
hazerman <49827421+hazerman@users.noreply.github.com>
2020-07-14 21:27:17 +0500

    Обновляет build

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,24 @@
 'use strict';
 
 (function () {
+  var questionsArea = document.querySelector('.questions__list');
+
+  var toggleText = function (item) {
+    item.classList.toggle('questions__item--show');
+  };
+
+  questionsArea.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    var target = evt.target.closest('button');
+    if (target) {
+      toggleText(target.closest('.questions__item'));
+    }
+  });
+})();
+
+'use strict';
+
+(function () {
   var popupForm = document.querySelector('#popup-form');
 
   popupForm.addEventListener('submit', function (evt) {
@@ -68,6 +86,50 @@
   window.popup = {
     remove: removePopup
   };
+})();
+
+'use strict';
+
+(function () {
+  var commentList = document.querySelector('.review__comment-list');
+  var commentItem = commentList.querySelectorAll('li');
+  var photoList = document.querySelector('.review__photo-list');
+  var photoItem = photoList.querySelectorAll('li');
+  var actionArea = document.querySelector('.review__slider-action');
+  var prevButton = actionArea.querySelector('.review__button--prev');
+  var nextButton = actionArea.querySelector('.review__button--next');
+  var counter = document.querySelector('#review-current');
+  var slidesCount = commentItem.length;
+  var currentSlide = 2;
+
+  var changeSlide = function (direction) {
+    commentItem[currentSlide].classList.remove('review__comment-item--current');
+    photoItem[currentSlide].classList.remove('review__photo-item--current');
+    if (direction) {
+      currentSlide++;
+      if (currentSlide > slidesCount - 1) {
+        currentSlide = 0;
+      }
+    } else {
+      currentSlide--;
+      if (currentSlide < 0) {
+        currentSlide = slidesCount - 1;
+      }
+    }
+    commentItem[currentSlide].classList.add('review__comment-item--current');
+    photoItem[currentSlide].classList.add('review__photo-item--current');
+    counter.innerText = currentSlide + 1;
+  };
+
+  prevButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    changeSlide(false);
+  });
+
+  nextButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    changeSlide(true);
+  });
 })();
 
 'use strict';
