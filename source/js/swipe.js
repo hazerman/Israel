c3cebd4590b4c345a1806dbb140d89f1effe39dd
hazerman <49827421+hazerman@users.noreply.github.com>
2020-07-21 19:00:00 +0500

    Исправляет проблемы с переключением слайдов

--- source/js/swipe.js
+++ source/js/swipe.js
@@ -1,9 +1,12 @@
 'use strict';
 
 (function () {
+  var MAX_MOBILE = 767;
+  var MOBILE_MEDIA = '(max-width:' + MAX_MOBILE + 'px)';
   var SWIPE_MIN = 60;
-  var swipeArea = document.querySelector('#swipe-slider');
-  var slides = swipeArea.querySelectorAll('[data-swipe-slide]');
+  var swipeSlider = document.querySelector('#swipe-slider');
+  var swipeArea = swipeSlider.parentElement;
+  var slides = swipeSlider.querySelectorAll('[data-swipe-slide]');
   var dotList = document.querySelector('.life__dot-list');
   var dotItems = dotList.querySelectorAll('.life__dot-item');
   var currentSlide = 0;
@@ -12,22 +15,25 @@
     return evt.changedTouches ? evt.changedTouches[0] : evt;
   };
 
-  var changeSlide = function (direction) {
+  var changeSlide = function (direction, needReset) {
     slides[currentSlide].dataset.swipeSlide = 'false';
     dotItems[currentSlide].classList.remove('life__dot-item--current');
-    if (direction) {
+    if (direction && !needReset) {
       currentSlide++;
-    } else {
+    } else if (!direction && !needReset) {
       currentSlide--;
+    } else if (needReset) {
+      currentSlide = 0;
     }
-    swipeArea.style.transform = 'translateX(-' + currentSlide * 100 + '%)';
+    swipeSlider.style.transform = 'translateX(-' + currentSlide * 100 + '%)';
     slides[currentSlide].dataset.swipeSlide = 'true';
     dotItems[currentSlide].classList.add('life__dot-item--current');
   };
 
-  swipeArea.addEventListener('touchstart', function (evt) {
+  var onSwipeAreaActionStart = function (evt) {
     var startPosition = unify(evt).pageX;
-    var onDocumentTouchEnd = function (endEvt) {
+
+    var onDocumentActionEnd = function (endEvt) {
       var shift = startPosition - unify(endEvt).pageX;
       if (Math.abs(shift) >= SWIPE_MIN) {
         if (shift > 0 && currentSlide !== slides.length - 1) {
@@ -36,8 +42,34 @@
           changeSlide(false);
         }
       }
-      document.removeEventListener('touchend', onDocumentTouchEnd);
+      document.removeEventListener('touchend', onDocumentActionEnd);
+      document.removeEventListener('mouseup', onDocumentActionEnd);
     };
-    document.addEventListener('touchend', onDocumentTouchEnd);
+
+    document.addEventListener('touchend', onDocumentActionEnd);
+    document.addEventListener('mouseup', onDocumentActionEnd);
+  };
+
+  var addSwipeAreaListeners = function () {
+    swipeArea.addEventListener('touchstart', onSwipeAreaActionStart);
+    swipeArea.addEventListener('mousedown', onSwipeAreaActionStart);
+  };
+
+  var removeSwipeAreaListeners = function () {
+    swipeArea.removeEventListener('touchstart', onSwipeAreaActionStart);
+    swipeArea.removeEventListener('mousedown', onSwipeAreaActionStart);
+    changeSlide(true, true);
+  };
+
+  if (window.matchMedia(MOBILE_MEDIA).matches) {
+    addSwipeAreaListeners();
+  }
+
+  window.addEventListener('resize', function () {
+    if (window.innerWidth <= MAX_MOBILE) {
+      addSwipeAreaListeners();
+    } else {
+      removeSwipeAreaListeners();
+    }
   });
 })();
